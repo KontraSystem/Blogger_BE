bb67ab0e98ada723d0009634fc5f6bc099975f6f
LukaKontrec <lk1574@g.rit.edu>
2023-01-15 22:00:56 +0100

    new netlify commint

--- src/postgres.js
+++ src/postgres.js
@@ -2,7 +2,7 @@ const pg = require('pg')
 
 const client = new pg.Client(process.env.DB_URL)
 client.connect(() => {
-	console.log(err ?? "Connected")
+	console.log("Connected")
 })
 
 module.exports = client
