7d8ff7a10ab24d095fdc7ef0acbf704db315133b
LukaKontrec <lk1574@g.rit.edu>
2023-01-15 23:45:31 +0100

    remove console.log

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,12 +5,12 @@ const client = require('../postgres');
 exports.getUserData = async(req, res) => {
 	const id = req.query.id
 	const query = await client.query('SELECT id, username, email, created_at FROM public.users u WHERE u.id=$1', [id])
-	console.log(query.rows, id)
+
 	res.send(query.rows[0])
 }
 
 exports.resetPassword = async (req, res) => {
-	console.log(req)
+
 	const query = await client.query(
 		"UPDATE public.users u SET password_hash=encode(digest(concat(u.password_salt, '"+req.body.pass+"'), 'sha256'), 'hex') WHERE u.id=$1",
 		[req.body.id]
@@ -23,8 +23,7 @@ exports.resetPassword = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
 	const query = await client.query("UPDATE public.users u SET username=$1, email=$2 WHERE u.id=$3", [req.body.username, req.body.email, req.body.id])
-	
-	console.log(query.rows)
+
 	res.send({
 		success: true
 	})
@@ -33,7 +32,7 @@ exports.updateUser = async (req, res) => {
 exports.register =  async (req, res) => {
 
 	const query = await client.query('SELECT public.f_create_user($1, $2, $3)', [req.body.username, req.body.password, req.body.email])
-	console.log(query.rows[0])
+
 	const { success, data } = query.rows[0].f_create_user
 	if (success) { 
 		const { id, email } = data
